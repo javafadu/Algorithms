68c7c021e161d78714bd35c46e40c26c8eb0f508
javafadu <javafadu@gmail.com>
2023-05-11 00:54:30 +0300

    11May-0030

--- src/ShellSort/ShellSortRunner.java
+++ src/ShellSort/ShellSortRunner.java
@@ -1,14 +1,14 @@
-package MergeSort;
+package ShellSort;
 
 import java.util.Arrays;
 
-public class MergeSortRunner {
+public class ShellSortRunner {
 
     public static void main(String[] args) {
         int[] arr = {5,3,4,2};
 
         System.out.println("Before Sorting : " + Arrays.toString(arr));
-        MergeSort.mergeSort(arr,0,arr.length-1);
+        ShellSort.shellSort(arr);
         System.out.println("After Sorting : " + Arrays.toString(arr));
     }
 }
